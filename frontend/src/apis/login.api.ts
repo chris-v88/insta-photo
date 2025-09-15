9becbb9ca965a319e5a948dd8d9849d34b81d90a
Chris V. <vochris81@gmail.com>
2025-09-15 14:03:58 -0400

    Revert "Revert "fix zod errors""

--- frontend/src/apis/login.api.ts
+++ frontend/src/apis/login.api.ts
@@ -9,8 +9,12 @@ export type LoginUserPayload = {
 export const postLoginUser = async (data: LoginUserPayload) => {
   try {
     const rawResponse = await axiosInstance.post('/auth/login', data);
-    const response = dataUserSchema.parse(rawResponse.data);
-    return response;
+    console.log('Login response:', rawResponse.data);
+    if (rawResponse.data && rawResponse.data.data && rawResponse.data.data.user) {
+      return dataUserSchema.parse(rawResponse.data.data.user);
+    } else {
+      throw new Error('No user data in response');
+    }
   } catch (err) {
     console.error('Login API error:', err);
   }
